6e3a75412123a20b4b2196c4805cd0d928b9f677
DineshMadhavanM <kit27.ad17@gmail.com>
2026-03-06 09:47:16 +0530

    Restore MongoDB connection and clean up debug logs in server

--- server/index.cjs
+++ server/index.cjs
@@ -9,14 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Simple logger
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
-
 const MONGO_URI = process.env.MONGODB_URI;
-
 mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB Connected via Environment Variable'))
     .catch(err => console.log('MongoDB Connection Error:', err));
